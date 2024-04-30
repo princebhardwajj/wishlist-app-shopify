9567730f8abfcea994667b6f5d111762afee66bf
princebhardwajj <prince.twotechies@gmail.com>
2024-04-30 17:19:10 +0530

    loader and api handling done next vid15

---
+++ app/routes/app.settings.jsx
@@ -0,0 +1,96 @@
+import {
+  Box,
+  Card,
+  Layout,
+  Link,
+  List,
+  Page,
+  Text,
+  BlockStack,
+  InlineGrid,
+  TextField,
+  useBreakpoints,
+  Divider,
+  Button,
+} from "@shopify/polaris";
+import { useState } from "react";
+import { json } from "@remix-run/node";
+import { useLoaderData, Form } from "@remix-run/react";
+
+export async function loader(){
+  // get data from database
+  let settings = {
+    name: "My app",
+    description: "My app Description",
+  }
+
+  return await json(settings); 
+}
+
+export async function action({request}) {
+  // updates persistent data
+  let settings = await request.formData();
+  settings = Object.fromEntries(settings);
+  return json(settings)
+}
+
+
+export default function SettingsPage() {
+
+  const settings = useLoaderData();
+  console.log(settings);
+
+  const [formState, setFormState] = useState(settings);
+
+  return (
+    <Page>
+      <ui-title-bar title="Settings" />
+      <BlockStack gap={{ xs: "800", sm: "400" }}>
+        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
+          <Box
+            as="section"
+            paddingInlineStart={{ xs: 400, sm: 0 }}
+            paddingInlineEnd={{ xs: 400, sm: 0 }}
+          >
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                Settings
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Update app setting and preferences.
+              </Text>
+            </BlockStack>
+          </Box>
+          <Card roundedAbove="sm">
+            <Form method="post">
+              <BlockStack gap="400">
+              <TextField label="App name" name="name" value={formState?.name || 'Wishlist Inspire'} onChange={(value) => setFormState({...formState, name: value}) } />
+
+                <TextField label="Description" name="description" value={formState?.description || 'Wishlist Inspire Description'} onChange={(value) => setFormState({...formState, description: value}) } />
+                <Button submit={true} >Save</Button>
+              </BlockStack>
+            </Form>
+          </Card>
+        </InlineGrid>
+
+      </BlockStack>
+    </Page>
+  );
+}
+
+function Code({ children }) {
+  return (
+    <Box
+      as="span"
+      padding="025"
+      paddingInlineStart="100"
+      paddingInlineEnd="100"
+      background="bg-surface-active"
+      borderWidth="025"
+      borderColor="border"
+      borderRadius="100"
+    >
+      <code>{children}</code>
+    </Box>
+  );
+}
