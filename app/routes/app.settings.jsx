d32537fd03f05fc55b8a26c6406d9077dde239b9
princebhardwajj <prince.twotechies@gmail.com>
2024-07-01 17:15:20 +0530

    wishlist adding app name errror solved

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -35,15 +35,15 @@ export async function action({request}) {
   //upadte database
   await db.settings.upsert({
     where: {
-      id: '1'
+      id: 1
     },
     update: {
-      id: '1',
+      id: 1,
       name: settings.name,
       description: settings.description
     },
     create: {
-      id: '1',
+      id: 1,
       name: settings.name,
       description: settings.description
     }
